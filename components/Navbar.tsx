0f6b3aedd9e855c711d9560894bd92a9deca41fd
abhay-raj19 <rajpootabhay423@gmail.com>
2024-06-12 07:24:02 +0530

    feat:Next_auth
    Signed-off-by: abhay-raj19 <rajpootabhay423@gmail.com>

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ import MaxwidthWrapper from "./MaxwidthWrapper";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
-const Navbar = () => {
-  const user = undefined;
-  const isAdmin = undefined;
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser()
+  const isAdmin = user?.email ===process.env.ADMIN_EMAIL;
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxwidthWrapper>
